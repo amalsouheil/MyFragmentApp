69e24d09dd7a1222240e7b2874cec620f6a2bc1a
Amal <amalsouheil@gmail.com>
2019-02-06 15:46:20 +0100

    l'optimisation de la stabilité et les performances de mon  application à base de fragments, sur smartphone et sur tablette.

--- app/src/main/java/amal/souheil/myfragmentapp/fragments/DetailFragment.java
+++ app/src/main/java/amal/souheil/myfragmentapp/fragments/DetailFragment.java
@@ -6,13 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import amal.souheil.myfragmentapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
+
 public class DetailFragment extends Fragment {
+
+
+    // 1 - Declare TextView
+    private TextView textView;
+
+    private int buttonTag;
+    public static final String KEY_BUTTONTAG = "souheil.amal.myfragmentapp.controllers.fragments.DetailFragment.KEY_BUTTONTAG";
+
 
 
     public DetailFragment() {
@@ -24,7 +35,62 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail2, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        // 2 - Get textView from layout (don't forget to create ID in fragment_detail.xml)
+
+        this.textView = (TextView) view.findViewById(R.id.fragment_detail_text_view);
+        return(view);
+
     }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // 5 - Restore last buttonTag if possible
+        if (savedInstanceState != null) {
+            int buttonTagRestored = savedInstanceState.getInt(KEY_BUTTONTAG, 0);
+            // 6 - Update TextView
+            this.updateTextView(buttonTagRestored);
+        }
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // 4 - Save buttonTag in Bundle when fragment is destroyed
+        outState.putInt(KEY_BUTTONTAG, buttonTag);
+    }
+
+
+
+
+
+
+    // 3 - Update TextView depending on TAG's button
+    public void updateTextView(int tag){
+
+        // 3 - Save tag in ButtonTag variable
+
+        this.buttonTag=tag;
+
+
+        switch (tag){
+            case 10:
+                this.textView.setText("You're a very good programmer !");
+                break;
+            case 20:
+                this.textView.setText("I do believe that Jon Snow is going to die in next season...");
+                break;
+            case 30:
+                this.textView.setText("Maybe Game of Thrones next season will get back in 2040 ?");
+                break;
+            default:
+                break;
+        }
+    }
+
 
 }
